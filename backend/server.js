30236ac0d59c87c1e381bd46fac9e602a7799e19
mayankyadav19 <hatmario06@gmail.com>
2026-06-28 03:29:09 +0530

    enable cors for all types of requests

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
